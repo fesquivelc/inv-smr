99e43cfbcf90784081c199076933ff147750599d
Documentos <Documentos@Doc-PC>
2014-09-16 22:01:06 -0500

    test

---
+++ src/main/java/com/proyecto/control/TipoUsuarioControlador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.proyecto.control;
+
+import com.proyecto.beans.TipoUsuario;
+
+/**
+ *
+ * @author Documentos
+ */
+public class TipoUsuarioControlador extends AbstractControlador<TipoUsuario>{
+
+    public TipoUsuarioControlador() {
+        super(TipoUsuario.class);
+    }
+    
+    
+}
